239aaee308baf58ffff0d928b63f5ce7ef4a1a51
Angrymouse <rykov.nikas@gmail.com>
2023-06-22 01:14:51 +0300

    fix contracts under id endpoint

--- routes/contractsUnderCode.js
+++ routes/contractsUnderCode.js
@@ -1,7 +1,8 @@
 const fp = require('fastify-plugin')
 module.exports = fp(async function (app, opts) {
     app.get("/contracts-under-code/:codeId", async (req, resp) => {
-        let contracts = await databases.contracts.getRange().filter(c => c.tags.find(tag => tag.name == "Contract-Src").value == req.params.codeId).map(c => c.id)
+        let contracts = await databases.contracts.getRange().filter(c => c.value.tags.find(tag => tag.name == "Contract-Src").value == req.params.codeId).map(c => c.value.id)
+
         if (!contracts) {
             resp.status(404)
             return "Not found"
