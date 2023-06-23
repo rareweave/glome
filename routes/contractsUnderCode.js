215f40d19d69263a2a9a131df1b3b13797023904
Angrymouse <rykov.nikas@gmail.com>
2023-06-23 08:54:17 +0300

    expand states

--- routes/contractsUnderCode.js
+++ routes/contractsUnderCode.js
@@ -1,13 +1,17 @@
 const fp = require('fastify-plugin')
 module.exports = fp(async function (app, opts) {
     app.get("/contracts-under-code/:codeId", async (req, resp) => {
-        let contracts = await databases.contracts.getRange().filter(c => c.value.tags.find(tag => tag.name == "Contract-Src").value == req.params.codeId).map(c => c.value.id)
+        let contracts = await databases.contracts.getRange({ offset: req.query.offset || 0, limit: Math.min(req.query.limit || 300, 300) }).filter(c => c.value.tags.find(tag => tag.name == "Contract-Src").value == req.params.codeId).map(c => c.value.id)
 
         if (!contracts) {
             resp.status(404)
             return "Not found"
         } else {
-            return contracts
+            if (!req.query.expandStates) {
+                return contracts
+            } else {
+                return (await databases.evaluationResults.getMany(contracts.map(c => c.id + "latest"))).map((er, contractIndex) => ({ state: er.state, contractId: contracts[contractIndex] }))
+            }
         }
     })
 })
